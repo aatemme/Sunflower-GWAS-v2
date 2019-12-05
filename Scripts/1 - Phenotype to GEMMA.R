3449f55cacb51b0c4da0ab14817ccd83d96b4a28
aatemme <atemme@uga.edu>
2019-12-05 09:25:07 -0500

    Remove bad code and fix bug

--- Scripts/1 - Phenotype to GEMMA.R
+++ Scripts/1 - Phenotype to GEMMA.R
@@ -5,21 +5,6 @@
 ##### Algorithm: GEMMA
 library(data.table)
 
-# ##### first run, convert the .tped and .tfam files into .bed, .bim, and .fam
-#   wd<-getwd()
-#   setwd("Software") #poor practices but typing out the path for the "system" command is a bother
-#   system("./plink --tped XRQv1_412_239_filtered.tped --tfam XRQv1_412_239_filtered.tfam --recode --make-bed --out XRQv1_412_239_filtered --allow-extra-chr")
-#   ### Then create the relatedness matrix
-#   fam<-fread("XRQv1_412_239_filtered.fam")
-#   fam$V6<-c(rep(1,dim(fam)[1]))   #### this is crucial as GEMMA will not compute relatedness if the phenotype is missing (-9)
-#   write.table(file="XRQv1_412_239_filtered.fam",fam,col.names=F, row.names=F, quote =F)
-#   system("./gemma -bfile XRQv1_412_239_filtered -gk 1 -o XRQv1_412_239_filtered")
-#   structure<-fread("XRQv1_412_239_filtered.PCA_EV")
-#   write.table(file="XRQv1_412_filtered.PCA_EV",structure ,row.names=F,col.names=F)
-#   setwd(wd)
-# #########
-
-
 ## Read in traits and environments to run
 envs<-as.character(read.table("environments_to_run.txt")[,1])
 traits<-as.character(read.table("traits_to_run.txt")[,1])
@@ -48,7 +33,7 @@ trait.data<-pheno.data[, ..select_cols]
 
 fam.file<-fread("XRQv1_412_239_filtered.fam")
 fam.file$V6<-NULL
-fam.file <- merge(fam.file,trait.data,by.x="V1",by.y="SAM")
+fam.file <- merge(fam.file,trait.data,by.x="V1",by.y="SAM",all.x=T)
 write.table(file="XRQv1_412_239_filtered.fam",fam.file,col.names=F, row.names=F, quote =F)
 
 
